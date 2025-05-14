b250cecd1bbbdb4c3a3e716eda87ad4c0442ea74
parksehun <davidpark8575@gmail.com>
2025-05-14 16:12:30 +0900

    1. 파일 업로드 및 저장 기능 추가 2. requirements.txt 수정

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from app.routers import user
+from app.routers import user, upload
 app = FastAPI()
 # uvicorn app.main:app --reload
 
 app.include_router(user.router)
-
+app.include_router(upload.router)
 @app.get("/")
 def root():
     return {
